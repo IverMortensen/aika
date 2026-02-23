d249911de67992a23be9c37539856250d74af9e8
Iver Mortensen <iver.mortensen@gmail.com>
2026-02-23 20:42:55 +0100

    Added simple http server to initial agent.

--- internal/agents/initial.go
+++ internal/agents/initial.go
@@ -2,40 +2,70 @@ package agents
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/IverMortensen/aika/internal/queue"
-	"github.com/IverMortensen/aika/internal/server"
 )
 
 type InitialBehavior struct {
 	imageDir string
 	queue    *queue.Queue
-	server   *server.Server
+	server   *http.Server
 }
 
 func NewInitialBehavior(imageDir string, queuePath string, serverAddress string) (*InitialBehavior, error) {
+	ib := &InitialBehavior{imageDir: imageDir}
+
 	// Create the persistent queue
 	pq, err := queue.NewPersistentQueue()
 	if err != nil {
 		return nil, fmt.Errorf("Failed to create queue: %w", err)
 	}
+	ib.queue = pq
 
 	// Create the server
-	server, err := server.NewServer(serverAddress)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to create server: %w", err)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/claim", ib.handleClaim)
+
+	ib.server = &http.Server{
+		Addr:    serverAddress,
+		Handler: mux,
 	}
 
-	return &InitialBehavior{
-		imageDir: imageDir,
-		queue:    pq,
-		server:   server,
-	}, nil
+	return ib, nil
+}
+
+func (ib *InitialBehavior) handleClaim(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Received request: %v", r)
+
+	w.Header().Set("Content-type", "application/json")
+
+	json.NewEncoder(w).Encode(`{"res":"RESPONSE!!"}`)
 }
 
 func (ib *InitialBehavior) Run(ctx context.Context) error {
-	log.Println("Running...")
+	// Add images to queue
+
+	// Start server
+	go func() {
+		if err := ib.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("Server error: %v", err)
+		}
+	}()
+
+	// Handle requests
+
+	// Wait for context to cancel
+	<-ctx.Done()
+
+	// Shutdown server
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	ib.server.Shutdown(shutdownCtx)
+
 	return nil
 }
