7b3081ecfe0a24456f5b8ac81d6eba9f45a04fa3
Iver Mortensen <iver.mortensen@gmail.com>
2026-02-25 15:14:34 +0100

    Bare bones working implementation of initial agent.

--- internal/agents/initial.go
+++ internal/agents/initial.go
@@ -2,26 +2,26 @@ package agents
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/IverMortensen/aika/internal/queue"
 )
 
 type InitialBehavior struct {
-	imageDir string
-	queue    *queue.Queue
-	server   *http.Server
+	imagePath string
+	queue     *queue.PersistentQueue
+	server    *http.Server
 }
 
 func NewInitialBehavior(imageDir string, queuePath string, serverAddress string) (*InitialBehavior, error) {
-	ib := &InitialBehavior{imageDir: imageDir}
+	ib := &InitialBehavior{imagePath: imageDir}
 
 	// Create the persistent queue
-	pq, err := queue.NewPersistentQueue()
+	pq, err := queue.NewPersistentQueue(imageDir)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to create queue: %w", err)
 	}
@@ -42,30 +42,43 @@ func NewInitialBehavior(imageDir string, queuePath string, serverAddress string)
 func (ib *InitialBehavior) handleClaim(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Received request: %v", r)
 
-	w.Header().Set("Content-type", "application/json")
+	// Get the next file
+	image, err := ib.queue.Pop()
+	if err != nil {
+		log.Printf("Failed to read next image name: %v", err)
+	}
 
-	json.NewEncoder(w).Encode(`{"res":"RESPONSE!!"}`)
+	// Mark file as in progress
+
+	// Send file name
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"image_name": "%s"}`, image)
 }
 
 func (ib *InitialBehavior) Run(ctx context.Context) error {
+	// Get the image directory
+	_, err := os.Open(ib.imagePath)
+	if err != nil {
+		log.Printf("Failed to read image directory: %v", err)
+	}
+
 	// Add images to queue
 
 	// Start server
 	go func() {
 		if err := ib.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Printf("Server error: %v", err)
+			log.Printf("Failed to start server: %v", err)
 		}
 	}()
-
-	// Handle requests
 
 	// Wait for context to cancel
 	<-ctx.Done()
 
-	// Shutdown server
+	// Shutdown server and close the queue
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	ib.server.Shutdown(shutdownCtx)
+	ib.queue.Close()
 
 	return nil
 }
