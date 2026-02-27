70fd80ef1478ee54135120aa8437e0cead183ca7
Iver Mortensen <iver.mortensen@gmail.com>
2026-02-27 17:22:49 +0100

    Rough working agent flow. Updated worker agent to loop their actions, handle EOF from initial agent and to send results to final agent.

--- internal/agents/worker.go
+++ internal/agents/worker.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os/exec"
@@ -30,21 +31,28 @@ func (wb *WorkerBehavior) Run(ctx context.Context) error {
 	// Work loop:
 	//	Request work from initial agent
 	//	 If no work: break
-	imgPath, err := wb.getImgPath()
-	if err != nil {
-		log.Printf("Failed to get image path from initial server: %v", err)
-		return nil
-	}
+	// TODO: Find a way to handle the errors in the loop.
+	// Should probably say something to the initial agent
+	for {
+		imgPath, err := wb.getImgPath()
+		if err != nil {
+			log.Printf("Failed to get image path from initial server: %v", err)
+			return err
+		} else if err == io.EOF { // No more images
+			break
+		}
 
-	//	Do the work
-	res, err := runModel(imgPath)
-	if err != nil {
-		log.Printf("Failed to run model: %v", err)
-		return nil
-	}
+		//	Do the work
+		res, err := runModel(imgPath)
+		if err != nil {
+			log.Printf("Failed to run model: %v", err)
+			return err
+		}
 
-	//	Send result to final agent
-	log.Printf("Result: %v", res)
+		//	Send result to final agent
+		wb.postLabel(res, imgPath)
+		log.Printf("Result: %v", res)
+	}
 
 	return nil
 }
@@ -65,12 +73,40 @@ func (wb *WorkerBehavior) getImgPath() (string, error) {
 		return "", fmt.Errorf("Failed to decode response: %v", err)
 	}
 
+	// Check if there are more images to process
+	if result["eof"] == "true" {
+		return "", io.EOF
+	}
+
 	imgPath, ok := result["image_path"]
 	if !ok {
 		return "", fmt.Errorf("No 'image_path' in response.")
 	}
 
 	return imgPath, nil
+}
+
+// Send the label and the path of a classified image to a final agent
+func (wb *WorkerBehavior) postLabel(imgPath string, label string) error {
+	fa_url := "http://" + wb.faAddress + "/submit"
+
+	// Format message containing path and label into json
+	data := map[string]string{"image_path": imgPath, "label": label}
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Errorf("Failed to encode result: %v", err)
+	}
+
+	// Send message
+	resp, err := http.Post(fa_url, "application/json", bytes.NewBuffer(jsonBytes))
+	if err != nil {
+		return fmt.Errorf("Failed to send label: %v", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Unexpected status code: %v", resp.StatusCode)
+	}
+
+	return nil
 }
 
 func runModel(imgPath string) (string, error) {
