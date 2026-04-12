e9af16e47f8ea089de5e5a237e35db3eeb67b287
Iver Mortensen <iver.mortensen@gmail.com>
2026-04-12 19:57:21 +0200

    Fixed small isse in kill script.

--- scripts/kill_nodes.sh
+++ scripts/kill_nodes.sh
@@ -8,5 +8,5 @@ fi
 for node in "$@"; do
     echo "Killing inf_3203 processes on $node..."
     ssh -o ConnectTimeout=5 -o StrictHostKeyChecking=no "$node" \
-        "pkill -u imo059 -f inf_3203 && echo 'Done' || echo 'No matching processes found'"
+        "pkill -u $USER -f inf_3203 && echo 'Done' || echo 'No matching processes found'"
 done
